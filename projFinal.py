32a086cebf11579a7bd5af14cc115d577d0f90f4
Mayara <rrddamazio@gmail.com>
2024-06-04 08:13:25 -0300

    mudanças

--- projFinal.py
+++ projFinal.py
@@ -1,3 +1,21 @@
-from flask import Flask, request
+from flask import Flask, request, render_template, abort
+from jinja2 import TemplateNotFound
 
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/index.html")
+def menu():
+    return render_template("index.html")
+
+@app.route('/cadastramento.html')
+def cadastra():
+    return render_template("cadastramento.html")
+
+@app.route('/ver_lista.html')
+def visualiza():
+    return render_template("ver_lista.html")
+    
+
+app.run(port=5002, debug=False)
 
