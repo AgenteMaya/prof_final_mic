75b53fe5e938f5544e28705f975e48fb48ce79c5
Mayara <rrddamazio@gmail.com>
2024-06-05 00:21:08 -0300

    pegar e baixar a foto do forms

--- projFinal.py
+++ projFinal.py
@@ -1,7 +1,14 @@
 from flask import Flask, request, render_template
+import os
+
 lAlunos = []
 
+UPLOAD_FOLDER = 'uploads'
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route("/")
 @app.route("/index.html")
@@ -13,9 +20,18 @@ def cadastra():
     if request.method == "POST":
         nome = request.form.get("fNome")
         matricula = request.form.get("fMatricula")
-        foto = request.form.get("fFoto")
-        lAlunos.append([nome, matricula, foto])
-        print("O aluno é " + nome + " e a matrícula dele é " + matricula + "e a foto dele é " + foto)
+        foto = request.files.get("fFoto")
+
+        if foto:
+            # Salvar a foto no diretório de uploads
+            foto_path = os.path.join(app.config['UPLOAD_FOLDER'], foto.filename)
+            foto.save(foto_path)
+            lAlunos.append([nome, matricula, foto.filename])
+        
+        else:
+            lAlunos.append([nome, matricula, None])
+        print("O aluno é " + nome + " e a matrícula dele é " + matricula + "e a foto dele é " + str(type(foto)))
+        print(lAlunos)
         return render_template("index.html")
     return render_template("cadastramento.html")
 
