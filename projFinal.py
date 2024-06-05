3a473f9f7f4e712eb3e2b465e249aa274e21ff97
Mayara <rrddamazio@gmail.com>
2024-06-04 22:54:47 -0300

    finalizar html e pegar nome e matricula do form pelo flask

--- projFinal.py
+++ projFinal.py
@@ -1,5 +1,5 @@
-from flask import Flask, request, render_template, abort
-from jinja2 import TemplateNotFound
+from flask import Flask, request, render_template
+lAlunos = []
 
 app = Flask(__name__)
 
@@ -8,14 +8,20 @@ app = Flask(__name__)
 def menu():
     return render_template("index.html")
 
-@app.route('/cadastramento.html')
+@app.route("/cadastramento.html", methods = ["GET", "POST"])
 def cadastra():
+    if request.method == "POST":
+        nome = request.form.get("fNome")
+        matricula = request.form.get("fMatricula")
+        foto = request.form.get("fFoto")
+        lAlunos.append([nome, matricula, foto])
+        print("O aluno é " + nome + " e a matrícula dele é " + matricula + "e a foto dele é " + foto)
+        return render_template("index.html")
     return render_template("cadastramento.html")
 
-@app.route('/ver_lista.html')
+@app.route("/ver_lista.html")
 def visualiza():
     return render_template("ver_lista.html")
-    
 
 app.run(port=5002, debug=False)
 
