c5d6184bc6539778cd8267b5b7c5899118436292
Mayara <rrddamazio@gmail.com>
2024-06-18 01:12:31 -0300

    Mudar de lista local para comunicacao com o mongoDB

--- projFinal.py
+++ projFinal.py
@@ -1,11 +1,23 @@
 from flask import Flask, request, render_template, redirect, url_for
 from pymongo import MongoClient, ASCENDING, DESCENDING
+from pymongo.server_api import ServerApi
 import os
 
-#cliente = MongoClient("localhost", 27017)
-#banco = cliente["banco_proj_final"]
-#colecao = banco["alunos"]
-lAlunos = []
+
+
+uri = "mongodb+srv://rrddamazio:vQ4lM2M1zErxlIFY@bdprojfinalmic.rgwiall.mongodb.net/?retryWrites=true&w=majority&appName=bdProjFinalMic"
+cliente = MongoClient(uri, 27017)
+"""
+try:
+    cliente.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+"""
+banco = cliente["banco_proj_final"]
+colecao = banco["alunos"]
+
+#lAlunos = []
 
 UPLOAD_FOLDER = 'static/uploads'
 if not os.path.exists(UPLOAD_FOLDER):
@@ -15,6 +27,7 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 # nome, matricula, curso, foto
+"""
 def busca(matricula):
     for (i, elem) in enumerate(lAlunos):
         if elem["matricula"] == matricula:
@@ -26,15 +39,21 @@ def busca(matricula):
         if elem["matricula"] == matricula:
             return i
     return None
+"""
 
-
-@app.route("/")
+@app.route("/") #mudado
 @app.route("/index.html")
 def menu():    
+    print(colecao.find())
+    lAlunos = []
+    print(lAlunos)
+    for elem in colecao.find():
+        print(elem)
+        lAlunos.append(elem)
     print(lAlunos)
     return render_template("index.html", lAlunos = lAlunos)
 
-@app.route("/cadastramento.html", methods = ["GET", "POST"])
+@app.route("/cadastramento.html", methods = ["GET", "POST"]) #mudado
 def cadastra():
     lCadastro = ["", "", ""]
     if request.method == "POST":
@@ -43,7 +62,10 @@ def cadastra():
         curso = request.form.get("fCurso")
         foto = request.files.get("fFoto")
 
-        ind = busca(matricula)
+        #ind = busca(matricula)
+        ind = colecao.find_one({"matricula" : matricula})
+        print(ind)
+        print(type(ind))
         if ind != None:           
             lCadastro[0] = nome
             lCadastro[1] = matricula
@@ -54,51 +76,69 @@ def cadastra():
             # Salvar a foto no diretório de uploads
             foto_path = os.path.join(app.config['UPLOAD_FOLDER'], foto.filename)
             foto.save(foto_path)    
-            lAlunos.append({"nome":nome, "matricula" : matricula, "curso" : curso, "foto" : foto.filename})        
-            #colecao.insert({"nome":nome, "matricula" : matricula, "curso" : curso, "foto" : foto.filename})
+            #lAlunos.append({"nome":nome, "matricula" : matricula, "curso" : curso, "foto" : foto.filename, "presenca" : []})        
+            colecao.insert_one({"nome":nome, "matricula" : matricula, "curso" : curso, "foto" : foto.filename, "presenca" : []})
         else:
-            lAlunos.append({"nome":nome, "matricula" : matricula, "curso" : curso, "foto" : None})
-            #colecao.insert({"nome":nome, "matricula" : matricula, "curso" : curso, "foto" : None})
+            #lAlunos.append({"nome":nome, "matricula" : matricula, "curso" : curso, "foto" : None, "presenca" : []})
+            colecao.insert_one({"nome":nome, "matricula" : matricula, "curso" : curso, "foto" : None, "presenca" : []})
         return redirect(url_for("menu"))
     else:
         return render_template("cadastramento.html", lCadastro = lCadastro)
     
-@app.route("/ver_lista.html")
+"""
+@app.route("/ver_lista.html") #mudado
 def visualiza():
-    return render_template("ver_lista.html", lAlunos = lAlunos)
+    print(colecao.find())
+    lAlunos = []
+    print(lAlunos)
+    for elem in colecao.find():
+        print(elem)
+        lAlunos.append(elem)
+    print(lAlunos)
 
-@app.route("/exclui/<num>.html", methods = ["GET", "POST"])
+    return render_template("ver_lista.html", lAlunos = lAlunos)
+"""
+
+@app.route("/exclui/<num>.html", methods = ["GET", "POST"]) #mudado
 def exclui(num):    
     
-    ind = busca(num)
-    lAlunos.pop(ind)
-        
+    #ind = busca(num)
+    #lAlunos.pop(ind)
+    colecao.delete_one({"matricula" : num})
+
     return redirect(url_for("menu"))
     
-@app.route("/edita/<num>.html", methods = ["GET", "POST"])
+@app.route("/edita/<num>.html", methods = ["GET", "POST"]) #mudado
 def edita(num):
     print(num)
-    ind = busca(num)
+    aluno = colecao.find_one({"matricula" : num})
+
     lEdita = ["", "", ""]
-    lEdita[0] = lAlunos[ind]["nome"]
-    lEdita[2] = lAlunos[ind]["curso"]
+    lEdita[0] = aluno["nome"]
+    lEdita[2] = aluno["curso"]
 
     if request.method == "POST":
         nome = request.form.get("fNome")
         curso = request.form.get("fCurso")
         foto = request.files.get("fFoto")
 
-        lAlunos[ind]["nome"] = nome
-        lAlunos[ind]["curso"] = curso
+        
         if foto:
             # Salvar a foto no diretório de uploads
             foto_path = os.path.join(app.config['UPLOAD_FOLDER'], foto.filename)
             foto.save(foto_path)
-            lAlunos[ind]["foto"] = foto.filename
+            colecao.update_one({"matricula" : num}, {"nome": nome, "matricula" : num, "curso" : curso, "foto" : foto.filename, "presenca" : aluno["presenca"]})
         else:    
-            lAlunos[ind]["foto"] = None
+            colecao.update_one({"matricula" : num}, {"nome": nome, "matricula" : num, "curso" : curso, "foto" : None, "presenca" : aluno["presenca"]})
+
         return redirect(url_for("menu"))
     return render_template("edita.html", lEdita = lEdita, num = num) 
     #return redirect(url_for("edita"))  
-app.run(port=5002, debug=False)
 
+@app.route("/presenca/<num>.html") #mudado
+def presenca(num):
+    #ind = busca(num)
+    aluno = colecao.find_one({"matricula" : num})
+    return render_template("presenca.html", lPresenca = aluno["presenca"]) 
+
+app.run(port=5002, debug=False)
