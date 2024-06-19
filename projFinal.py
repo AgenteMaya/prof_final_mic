7a0246fc4fa5a979a5104784121e878ef2b1839d
Mayara <rrddamazio@gmail.com>
2024-06-19 17:13:45 -0300

    Atualizacao aula urgente

--- projFinal.py
+++ projFinal.py
@@ -1,19 +1,14 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for, jsonify
 from pymongo import MongoClient, ASCENDING, DESCENDING
-from pymongo.server_api import ServerApi
+#from pymongo.server_api import ServerApi
 import os
-
+import json 
+import ast
 
 
 uri = "mongodb+srv://rrddamazio:vQ4lM2M1zErxlIFY@bdprojfinalmic.rgwiall.mongodb.net/?retryWrites=true&w=majority&appName=bdProjFinalMic"
 cliente = MongoClient(uri, 27017)
-"""
-try:
-    cliente.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
-"""
+
 banco = cliente["banco_proj_final"]
 colecao = banco["alunos"]
 
@@ -27,30 +22,47 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 # nome, matricula, curso, foto
-"""
-def busca(matricula):
-    for (i, elem) in enumerate(lAlunos):
-        if elem["matricula"] == matricula:
-            return i
-    return None
 
-def busca(matricula):
-    for (i, elem) in enumerate(lAlunos):
-        if elem["matricula"] == matricula:
-            return i
-    return None
-"""
+
+@app.route("/passaPresenca", methods = ["GET", "POST"])
+def passaPresenca():
+    if request.method == "POST":
+        arqJson = request.get_json()
+        print(arqJson)
+        print("entrei aqui")
+        
+        """
+        arqJson = json.loads(arqJson)
+        
+        lMatriculas = []
+        for aluno in arqJson["presenca"]:
+            lMatriculas.append(aluno["matricula"])
+
+        for aluno in colecao.find():
+            if aluno["matricula"] in lMatriculas:
+                colecao.update_one({"matricula" : aluno["matricula"]}, {"$set":{aluno["presenca"].append([arqJson["data"], "presente"])}})
+
+
+
+            for presente in arqJson["presenca"]:
+                if aluno["matricula"] == presente["matricula"]:
+                    #TODO tratamento do caso de atrasado ou não
+        """
+
+        return jsonify(arqJson)
+    return "não foi"
+                
+
+
+
+
 
 @app.route("/") #mudado
 @app.route("/index.html")
 def menu():    
-    print(colecao.find())
     lAlunos = []
-    print(lAlunos)
     for elem in colecao.find():
-        print(elem)
         lAlunos.append(elem)
-    print(lAlunos)
     return render_template("index.html", lAlunos = lAlunos)
 
 @app.route("/cadastramento.html", methods = ["GET", "POST"]) #mudado
@@ -85,32 +97,13 @@ def cadastra():
     else:
         return render_template("cadastramento.html", lCadastro = lCadastro)
     
-"""
-@app.route("/ver_lista.html") #mudado
-def visualiza():
-    print(colecao.find())
-    lAlunos = []
-    print(lAlunos)
-    for elem in colecao.find():
-        print(elem)
-        lAlunos.append(elem)
-    print(lAlunos)
-
-    return render_template("ver_lista.html", lAlunos = lAlunos)
-"""
-
 @app.route("/exclui/<num>.html", methods = ["GET", "POST"]) #mudado
-def exclui(num):    
-    
-    #ind = busca(num)
-    #lAlunos.pop(ind)
+def exclui(num):  
     colecao.delete_one({"matricula" : num})
-
     return redirect(url_for("menu"))
     
 @app.route("/edita/<num>.html", methods = ["GET", "POST"]) #mudado
 def edita(num):
-    print(num)
     aluno = colecao.find_one({"matricula" : num})
 
     lEdita = ["", "", ""]
@@ -121,24 +114,29 @@ def edita(num):
         nome = request.form.get("fNome")
         curso = request.form.get("fCurso")
         foto = request.files.get("fFoto")
-
         
         if foto:
-            # Salvar a foto no diretório de uploads
             foto_path = os.path.join(app.config['UPLOAD_FOLDER'], foto.filename)
             foto.save(foto_path)
-            colecao.update_one({"matricula" : num}, {"nome": nome, "matricula" : num, "curso" : curso, "foto" : foto.filename, "presenca" : aluno["presenca"]})
+            print({"matricula" : num}, {"nome": nome, "matricula" : num, "curso" : curso, "foto" : foto.filename, "presenca" : aluno["presenca"]})
+            colecao.update_one({"matricula" : num}, {"$set":{"nome": nome, "matricula" : num, "curso" : curso, "foto" : foto.filename, "presenca" : aluno["presenca"]}})
         else:    
-            colecao.update_one({"matricula" : num}, {"nome": nome, "matricula" : num, "curso" : curso, "foto" : None, "presenca" : aluno["presenca"]})
+            print({"matricula" : num}, {"nome": nome, "matricula" : num, "curso" : curso, "foto" : None, "presenca" : aluno["presenca"]})
+            colecao.update_one({"matricula" : num}, {"$set":{"nome": nome, "matricula" : num, "curso" : curso, "foto" : None, "presenca" : aluno["presenca"]}})
 
         return redirect(url_for("menu"))
     return render_template("edita.html", lEdita = lEdita, num = num) 
-    #return redirect(url_for("edita"))  
 
-@app.route("/presenca/<num>.html") #mudado
+@app.route("/presenca/<num>.html", methods = ["GET", "POST"]) #mudado
 def presenca(num):
     #ind = busca(num)
     aluno = colecao.find_one({"matricula" : num})
-    return render_template("presenca.html", lPresenca = aluno["presenca"]) 
+    lpresenca = []
+    print(aluno["presenca"])
+    for elemento in aluno["presenca"]:
+        elemento = ast.literal_eval(elemento)
+        lpresenca.append(elemento)
+    print(lpresenca)
+    return render_template("presenca.html", lpresenca = lpresenca) 
 
 app.run(port=5002, debug=False)
