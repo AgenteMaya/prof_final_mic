8e8a88575d85ab1ff108559880e7ed2b35e97efa
Mayara <rrddamazio@gmail.com>
2024-06-26 00:45:05 -0300

    Finalizar passaPresenca

--- projFinal.py
+++ projFinal.py
@@ -9,6 +9,17 @@ from datetime import datetime, time
 
 uri = "mongodb+srv://rrddamazio:vQ4lM2M1zErxlIFY@bdprojfinalmic.rgwiall.mongodb.net/?retryWrites=true&w=majority&appName=bdProjFinalMic"
 cliente = MongoClient(uri, 27017)
+'''
+try:
+    cliente = MongoClient(uri, 27017)
+    banco = cliente["banco_proj_final"]
+    colecao = banco["alunos"]
+    print("Conexão estabelecida com sucesso.")
+    for doc in colecao.find():
+        print(doc)
+except Exception as e:
+    print(f"Erro ao conectar com MongoDB: {e}")
+'''
 
 banco = cliente["banco_proj_final"]
 colecao = banco["alunos"] #colecaoALunos
@@ -23,51 +34,7 @@ if not os.path.exists(UPLOAD_FOLDER):
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-# nome, matricula, curso, foto
-
-
-@app.route("/passaPresenca", methods = ["GET", "POST"])
-def passaPresenca():
-    print("entrei na funcao")
-    #if request.method == "POST":
-        #arqJson = request.get_json()
-    arqJson = json.dumps({"data": "23/05/2024", "presencas": [{"matricula": 2210833, "hora": "23:59:05"}, {"matricula": 2210834, "hora": "00:59:05"}]})
-    print(arqJson)
-        # {"data : xx-xx-xxxx, "presencas" : [{"matricula" : xxx, "hora" : "xx:xx:xx"}]}
-        #{"data": "23/05/2024", "presencas": ["{"matricula": 2210833, "hora": "23:59:05"}, {"matricula": 2210834, "hora": "00:59:05"}]}
-    print("entrei aqui")   
-    arqJson = json.loads(arqJson)
-    print(arqJson)
-    
-    lMatriculas = []
-    lDatas = []
-    for aluno in arqJson["presenca"]:
-        lMatriculas.append(aluno["matricula"])
-        lDatas.append(datetime.strptime(aluno["hora"], "%M:%H:%S").time())
-
-    for aluno in colecao.find():
-        if aluno["matricula"] in lMatriculas:
-            ind = lMatriculas.index(aluno["matricula"])
-            horaAluno = lDatas[ind]
-            horaAula = datetime.strptime(colecaoDias.find_one({"dia" : "jhk"})["hora"], "%M:%H:%S").time()
-            if horaAluno <= horaAula:
-                presencaAluno = {""}
-            colecao.update_one({"matricula" : aluno["matricula"]}, {"$set":{aluno["presenca"].append([arqJson["data"], "presente"])}})
-
-
-
-        for presente in arqJson["presenca"]:
-            if aluno["matricula"] == presente["matricula"]:
-                #TODO tratamento do caso de atrasado ou não
-
-
-    return jsonify(arqJson)
-    return "não foi"
-                
-
-
-
-
+# nome, matricula, curso, foto            
 
 @app.route("/") #mudado
 @app.route("/index.html")
@@ -76,6 +43,8 @@ def menu():
     for elem in colecao.find():
         lAlunos.append(elem)
     return render_template("index.html", lAlunos = lAlunos)
+
+
 
 @app.route("/cadastramento.html", methods = ["GET", "POST"]) #mudado
 def cadastra():
@@ -143,13 +112,68 @@ def edita(num):
 def presenca(num):
     #ind = busca(num)
     aluno = colecao.find_one({"matricula" : num})
-    lpresenca = []
-    print(aluno["presenca"])
-    for elemento in aluno["presenca"]:
-        elemento = ast.literal_eval(elemento)
-        lpresenca.append(elemento)
-    print(lpresenca)
+    lpresenca = aluno["presenca"]
     return render_template("presenca.html", lpresenca = lpresenca) 
 
-if __name__ == '__main__':
-    app.run(port=5002, debug=False)
+
+
+@app.route("/criaAula", methods = ["GET", "POST"])
+def criaAula():
+    colecaoDias.insert_one({"data":"23-05-2024", "hora" : "23:00:00"})
+    return "entrou no bd"
+
+@app.route("/passaPresenca", methods = ["GET", "POST"])
+def passaPresenca():
+    #print("entrei na funcao")
+    if request.method == "POST":
+        arqJson = request.get_json()
+        #arqJson = json.dumps({"data": "23-05-2024", "presencas": [{"matricula": 2210833, "hora": "23:59:05"}, {"matricula": 2210834, "hora": "22:59:05"}]})
+        #print(arqJson)
+            # {"data : xx-xx-xxxx, "presencas" : [{"matricula" : xxx, "hora" : "xx:xx:xx"}]}
+            #{"data": "23/05/2024", "presencas": ["{"matricula": 2210833, "hora": "23:59:05"}, {"matricula": 2210834, "hora": "00:59:05"}]}
+        #print("entrei aqui")   
+        arqJson = json.loads(arqJson)
+        #print(arqJson)
+        
+        lMatriculas = []
+        lDatas = []
+        #print(arqJson["data"])
+        data = arqJson["data"]
+        data= datetime.strptime(arqJson["data"], "%d-%m-%Y")
+        data = data.strftime("%d-%m-%Y")
+        #print(data)
+        #print(arqJson["presencas"])
+        for aluno in arqJson["presencas"]:
+            lMatriculas.append(aluno["matricula"])
+            lDatas.append(datetime.strptime(aluno["hora"], "%H:%M:%S").time())
+
+        for aluno in colecao.find():
+            presencaAluno = []
+            if int(aluno["matricula"]) in lMatriculas:
+                ind = lMatriculas.index(int(aluno["matricula"]))
+                horaAluno = lDatas[ind]            
+                horaOficial = colecaoDias.find_one({"data" : data})
+                horaAula = datetime.strptime(horaOficial["hora"], "%H:%M:%S").time()
+                if horaAluno <= horaAula:
+                    presencaAluno = [data, "presente", "pontual"]
+                else:
+                    presencaAluno = [data, "presente", "atrasado"]
+            else:
+                presencaAluno = [data, "faltou", "-"]
+            
+            print(aluno["presenca"])
+            lAlunoPresenca = [ast.literal_eval(item) for item in aluno["presenca"]]
+            print(lAlunoPresenca)
+            lAlunoPresenca.append(presencaAluno)
+            print(lAlunoPresenca)
+
+            colecao.update_one({"matricula" : aluno["matricula"]}, {"$set":{"presenca" : lAlunoPresenca}})
+
+        return jsonify(arqJson)
+    return "não foi"
+
+@app.route("/passaInfo", methods = ["GET", "POST"])
+def passaInfo():
+    
+#if __name__ == '__main__':
+app.run(port=5002, debug=False)
